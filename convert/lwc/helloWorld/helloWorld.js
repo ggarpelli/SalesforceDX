98c7ebe54631869182dd29039d938e7e29f7e7dc
Guilherme Garpelli <ggarpelli@gmail.com>
2023-01-09 23:20:00 -0300

    Agora Vai, Deploy Scratch

--- convert/lwc/helloWorld/helloWorld.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-export default class helloWorld extends LightningElement {
-  greeting = 'World';
-  changeHandler(event) {
-    this.greeting = event.target.value;
-  }
-}
